1fca67bbcce9a217b8714ad4630e708ea69e5e59
Odin van Eijk <odinvaneijk@gmail.com>
2017-01-13 17:55:32 +0100

    versie 2 met database
    uitgewerkte versie 1/13/2017

--- app onsen.io/app.js
+++ app onsen.io/app.js
@@ -14,41 +14,73 @@ ons.ready(function() {
   });
 });
 
+
+document.addEventListener('init', function(event)
+{
+	if(event.target.id == "home"){
+		openDb();
+		getItems();
+	}
+});
+
 var db = null;
 function onError(tx, e)
 {
 	alert("Er ging iets fout! onze excuses" + e.Message);
 }
 
-function onSucces(tx, r);
+function onSucces(tx, r)
 {
-	
+	getItems();
 }
 
 function openDb()
 {
-db = openDatabase("gewichtlijst", "1", "gewicht lijst" 1024*1024);
+db = openDatabase("ShoppingList", "1", "shopping list" ,1024*1024);
 db.transaction(function(tx)
 {
-	tx.executeSql("CREATE TABLE IF NOT EXISTS gewicht (ID INTEGER PRIMARY KEY ASC, gewicht TEXT)", []);
+	tx.executeSql("CREATE TABLE IF NOT EXISTS items (ID INTEGER PRIMARY KEY ASC, item TEXT)", []);
 });
 	
 }
-function getGewicht()
+function getItems()
 {
 	db.transaction(function(tx){
-		tx.executeSql("SELECT * FROM gewicht",[], renderGewicht, onError);
+		tx.executeSql("SELECT * FROM items",[], renderItems, onError);
 	});
 }
 
-function renderGewicht(tx, rs)
+function renderItems(tx, rs)
 {
 	var output = "";
-	var list = document.getElementById('gewichtlijst');
+	var list = document.getElementById('shoppinglist');
 	
 	for (i = 0; i < rs.rows.length; i++)
 	{
-		
+		var row = rs.rows.item(i);
+		output+= "<ons-list-item>" + row.item +
+		"<div class=\"right\"> <ons-button onclick='deleteItem(" + row.ID + ");')> <ons-icon icon=\"trash\"> </ons-icon> </ons-button> </div>" +
+		"</ons-list-item>";
 	}
+	
+	list.innerHTML = output;
 }
 
+function addItem()
+{
+	var textbox = document.getElementById("item");
+	var value = textbox.value;
+	
+	db.transaction(function(tx) {
+		tx.executeSql("INSERT INTO items (item) VALUES (?)" , [value], onSucces, onError);
+	});
+	
+	textbox.value="";
+	//fn.load('home.html');
+}
+	function deleteItem(id)
+	{
+		db.transaction(function(tx){
+			tx.executeSql("DELETE FROM items WHERE ID=?", [id], onSucces, onError); 
+		});
+	}
---
+++ app onsen.io/theorie2.js
@@ -0,0 +1,11 @@
+<script>
+var product=1;
+var n=4;
+document.write(n+'!=');
+while(n>1){
+product=product*n;
+document.write(n+'x');
+n=n-1;
+};
+document.write('1 = '+product);
+</script>
